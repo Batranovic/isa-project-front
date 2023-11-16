d425dde5971cbbde0fb09e1afba6d7176f464b36
Nina <nina.batranovic3@gmail.com>
2023-11-16 17:16:21 +0100

    initial project folders

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AuthModule } from './infrastructure/auth/auth.module';
+import { LayoutModule } from './feature-modules/layout/layout.module';
+import { AppRoutingModule } from './infrastructure/routing/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthModule,
+    LayoutModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front-end/src/app/feature-modules/layout/layout.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    NavBarComponent
+  ],
+  imports: [
+    CommonModule
+  ], 
+  exports: [
+    HomeComponent,
+    NavBarComponent
+  ]
+})
+export class LayoutModule { }
