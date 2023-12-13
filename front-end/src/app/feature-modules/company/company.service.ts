cebddd84e1042a660417c3131734184ddf003681
Nina <nina.batranovic3@gmail.com>
2023-12-13 19:36:48 +0100

    authorization tried-succeded

--- front-end/src/app/feature-modules/company/company.service.ts
+++ front-end/src/app/feature-modules/company/company.service.ts
@@ -1,14 +1,15 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { SearchCompany } from './model/search-company.model';
 import { Company } from './model/company.model';
+import { AuthService } from 'src/app/infrastructure/auth/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CompanyService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
   getCompanies(): Observable<Company[]> {
     return this.http.get<Company[]>('http://localhost:8080/api/companies/all');
@@ -17,7 +18,12 @@ export class CompanyService {
   getFilteredCompanies(searchInputs:SearchCompany): Observable<Company[]> {
     return this.http.post<Company[]>('http://localhost:8080/api/companies/search',searchInputs);
   }
+  
   getCompanyDetails(companyId: number): Observable<Company> {
-    return this.http.get<Company>(`http://localhost:8080/api/companies/${companyId}`);
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.authService.getToken()}`, 
+    });
+
+    return this.http.get<Company>(`http://localhost:8080/api/companies/${companyId}`, { headers });
   }
 }
